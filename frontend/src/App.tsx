8ae12224aad152195b8f13cd5ccbd8d09acc3f18
Filipe <fgallo_dev@hotmail.com>
2022-11-15 08:53:04 -0300

    fix: server_url fetch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 // URL para o caminho "/home" do servidor
-const SERVER_URL = "http://localhost:3333/home";
+const SERVER_URL = "https://ng-cash-app-production.up.railway.app/home";
 
 function App() {
   const [data, setData] = React.useState<any>([]);
