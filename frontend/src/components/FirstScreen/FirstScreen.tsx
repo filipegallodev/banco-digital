b4d57215f3d4d8753e1805f97ac1b091bf3bb076
Filipe <fgallo_dev@hotmail.com>
2022-11-17 18:19:23 -0300

    firstscreen working and styles

--- frontend/src/components/FirstScreen/FirstScreen.tsx
+++ frontend/src/components/FirstScreen/FirstScreen.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import "./FirstScreen.css";
+
 const FirstScreen = () => {
   return (
-    <nav>
-      <NavLink to="login">Entrar</NavLink>
-      <NavLink to="register">Registrar</NavLink>
-    </nav>
+    <div className="nav-container">
+      <nav>
+        <h2 className="primary-title">Já possui uma conta?</h2>
+        <NavLink to="login">
+          <button className="login-register">Entrar</button>
+        </NavLink>
+        <h2 className="secundary-title">
+          Ainda não? Então não perca mais tempo! É{" "}
+          <strong>muito simples</strong> abrir uma conta com a gente.
+        </h2>
+        <NavLink to="register">
+          <button className="login-register">Registrar</button>
+        </NavLink>
+      </nav>
+    </div>
   );
 };
 
