a77cbee626e7abf3ae61d88ef336cad5c11d273c
Filipe <filipegallodev@hotmail.com>
2023-03-30 09:08:22 -0300

    feat: frontend tech transition: React.js -> Next.js

---
+++ frontend-nextjs/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Banco Digital</title>
+        <meta name="description" content="Bem-vindo ao seu Banco Digital!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Banco Digital</h1>
+      </main>
+    </>
+  );
+}
