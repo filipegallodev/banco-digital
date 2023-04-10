2023a64b43bd4efe37ab8a569c271ec3d2f3fe99
Filipe <fgallo_dev@hotmail.com>
2023-04-10 11:11:11 -0300

    feat: simple overview page

--- frontend-nextjs/src/pages/painel.tsx
+++ frontend-nextjs/src/pages/painel.tsx
@@ -3,6 +3,7 @@ import { useAppSelector } from "@/hooks/useAppSelector";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
+import styled from "styled-components";
 
 export default function Painel() {
   const { user } = useAppSelector((state: IReduxState) => state);
@@ -23,11 +24,58 @@ export default function Painel() {
       </Head>
       <Header />
       <main>
-        <div>
-          Painel | Olá, Sr{"("}a{")"} NOME | {user.data?.user.username} |{" "}
-          {user.data?.user.balance}
-        </div>
+        <Container>
+          <Title>Visão geral</Title>
+          <p>
+            Olá, Sr{"("}a{")"} NOME
+          </p>
+          <DashboardContainer>
+            <DashboardItem>Saldo: {user.data?.user.balance}</DashboardItem>
+            <DashboardItem>Transferências hoje:</DashboardItem>
+            <DashboardItem>Saída/Entrada</DashboardItem>
+          </DashboardContainer>
+        </Container>
       </main>
     </>
   );
 }
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  margin: 0 auto;
+  max-width: 1000px;
+`;
+
+const Title = styled.h1`
+  margin: 32px 0;
+  font-size: 2.5rem;
+  text-transform: uppercase;
+  box-sizing: border-box;
+  &::before {
+    content: "";
+    width: 32px;
+    height: 4px;
+    background-color: #c500d0;
+    position: absolute;
+  }
+`;
+
+const DashboardContainer = styled.div`
+  display: flex;
+  width: 100%;
+  border-radius: 8px;
+  overflow: hidden;
+  border: 1px solid #555;
+`;
+
+const DashboardItem = styled.div`
+  /* border: 1px solid #555; */
+  width: 100%;
+  height: 320px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
