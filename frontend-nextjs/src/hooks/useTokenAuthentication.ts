ffdc4e136af101bd495567e9bbc79dcc4a70e3db
Filipe <fgallo_dev@hotmail.com>
2023-04-12 10:54:57 -0300

    feat: custom hook for token authentication

---
+++ frontend-nextjs/src/hooks/useTokenAuthentication.ts
@@ -0,0 +1,30 @@
+import { useRouter } from "next/router";
+import { useAppSelector } from "./useAppSelector";
+import { useAppDispatch } from "./useAppDispatch";
+import { fetchToken, resetState } from "@/store/reducers/user";
+import { useEffect } from "react";
+
+export default function useTokenAuthentication() {
+  const { user } = useAppSelector((state: IReduxState) => state);
+  const { login } = useAppSelector((state: IReduxState) => state);
+  const route = useRouter();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwt-token");
+    if (!user.data?.validToken && token) dispatch(fetchToken(token));
+  }, [login]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("jwt-token");
+    if (user.error) {
+      dispatch(resetState());
+      route.push("/");
+    }
+    if (!token) {
+      route.push("/");
+    }
+  }, [user.error]);
+
+  return user;
+}
