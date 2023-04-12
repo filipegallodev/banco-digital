8e7f0f36f8893c7dfcd833557e980f6cba8eb0c8
Filipe <fgallo_dev@hotmail.com>
2023-04-12 10:57:14 -0300

    feat: new page for start a transference

---
+++ frontend-nextjs/src/pages/transferencias/nova/index.tsx
@@ -0,0 +1,51 @@
+import AuthPage from "@/components/AuthPage";
+import Header from "@/components/Header";
+import useTokenAuthentication from "@/hooks/useTokenAuthentication";
+import Head from "next/head";
+import React from "react";
+import styled from "styled-components";
+
+export default function Nova() {
+  const user = useTokenAuthentication();
+
+  if (!user.data) return <AuthPage />;
+  return (
+    <>
+      <Head>
+        <title>Painel | Banco Digital</title>
+        <meta name="description" content="Painel do seu Banco Digital!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main className="animeRight">
+        <Container>
+          <Title>Realizar Transferência</Title>
+        </Container>
+      </main>
+    </>
+  );
+}
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  margin: 0 auto;
+  max-width: 1200px;
+`;
+
+const Title = styled.h1`
+  margin: 48px 0;
+  font-size: 2.5rem;
+  text-transform: uppercase;
+  box-sizing: border-box;
+  &::before {
+    content: "";
+    width: 32px;
+    height: 4px;
+    background-color: #c500d0;
+    position: absolute;
+  }
+`;
