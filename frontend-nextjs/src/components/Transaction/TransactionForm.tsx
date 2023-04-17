50b408e40db488d41b7824557d63129813ac7697
Filipe <fgallo_dev@hotmail.com>
2023-04-17 11:32:42 -0300

    transaction form finished

--- frontend-nextjs/src/components/Transaction/TransactionForm.tsx
+++ frontend-nextjs/src/components/Transaction/TransactionForm.tsx
@@ -2,16 +2,35 @@ import React, { useEffect, useState } from "react";
 import CurrencyInput from "react-currency-input-field";
 
 const TransactionForm = () => {
+  const [transactionData, setTransactionData] = useState({
+    value: "",
+    target: "",
+  });
   const [transactionValue, setTransactionValue] = useState<string>();
 
   useEffect(() => {
-    console.log(transactionValue?.replace(",", ".").replace(/(\.$)/g, ""));
+    if (!transactionValue) return;
+    const formattedTransactionValue = transactionValue
+      .replace(",", ".")
+      .replace(/(\.$)/g, "");
+    setTransactionData({
+      ...transactionData,
+      value: formattedTransactionValue,
+    });
   }, [transactionValue]);
+
+  useEffect(() => {
+    console.log(transactionData);
+  }, [transactionData]);
+
+  function handleTransaction(event: React.FormEvent) {
+    event.preventDefault();
+  }
 
   return (
     <div>
       <h2>Preencha os campos necessários para realizar uma transação:</h2>
-      <form>
+      <form onSubmit={handleTransaction}>
         <label htmlFor="transaction-value">Quanto deseja transferir?</label>
         <CurrencyInput
           id="transaction-value"
@@ -22,7 +41,20 @@ const TransactionForm = () => {
           value={transactionValue}
           onValueChange={(value) => setTransactionValue(value)}
         />
-        {transactionValue}
+        <label htmlFor="transaction-target">
+          Para quem você deseja transferir essa quantia?
+        </label>
+        <input
+          type="text"
+          id="transaction-target"
+          name="transaction-target"
+          placeholder="email@exemplo.com"
+          onChange={({ target }) =>
+            setTransactionData({ ...transactionData, target: target.value })
+          }
+        />
+        Confirme os dados preenchidos.
+        <button>Enviar</button>
       </form>
     </div>
   );
