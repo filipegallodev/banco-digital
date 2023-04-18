c8a28e93d468e752913046d33d6fd1839b11bfaf
Filipe <filipegallodev@hotmail.com>
2023-04-18 14:52:47 -0300

    feat: helper function to format currency from a list

--- backend/src/helpers/currencyFormatter.ts
+++ backend/src/helpers/currencyFormatter.ts
@@ -1,12 +1,30 @@
-import { accounts } from "@prisma/client";
+import { Prisma, transactions } from "@prisma/client";
 
 export default function currencyFormatter(
   locale: string,
   currency: string,
-  userAccount: accounts | null
+  userBalance: Prisma.Decimal | string | undefined
 ) {
-  return Number(userAccount?.balance).toLocaleString(locale, {
+  return Number(userBalance).toLocaleString(locale, {
     style: "currency",
     currency: currency,
   });
+}
+
+interface ITransaction {
+  id: number;
+  debitedAccountId: number;
+  creditedAccountId: number;
+  value: string;
+  createdAt: Date;
+}
+
+export function currencyFormatterFromAList(list: any[]) {
+  const newList = list.map((transaction: ITransaction) => {
+    return {
+      ...transaction,
+      value: currencyFormatter("pt-BR", "BRL", transaction.value),
+    };
+  });
+  return newList;
 }
