89755bb8925e6dd89dc9b70de551bf138bc566db
Filipe <filipegallodev@hotmail.com>
2023-04-18 14:20:35 -0300

    feat: function to fetch transaction list

--- frontend-nextjs/src/store/reducers/transactions.ts
+++ frontend-nextjs/src/store/reducers/transactions.ts
@@ -30,9 +30,12 @@ const { fetchStarted, fetchSuccess, fetchError } = slice.actions;
 const SERVER_NEW_TRANSACTION_URL = "http://localhost:3333/transaction/create";
 // const SERVER_NEW_TRANSACTION_URL =
 //   "https://ng-cash-app-production.up.railway.app/transaction/create";
+const SERVER_TRANSACTIONS_LIST_URL = "http://localhost:3333/transaction/list";
+// const SERVER_NEW_TRANSACTION_URL =
+//   "https://ng-cash-app-production.up.railway.app/transaction/create";
 
 export const fetchTransaction =
-  (transactionData: ITransactionData) =>
+  (transactionData: ITransactionFormData) =>
   async (dispatch: Dispatch<Action<string>>) => {
     try {
       dispatch(fetchStarted());
@@ -43,6 +46,24 @@ export const fetchTransaction =
           Authorization: `${localStorage.getItem("jwt-token")}`,
         },
         body: JSON.stringify(transactionData),
+      });
+      const data = await response.json();
+      if (!response.ok) throw new Error(data.error);
+      dispatch(fetchSuccess(data));
+    } catch (err) {
+      if (err instanceof Error) dispatch(fetchError(err.message));
+    }
+  };
+
+export const fetchTransactionsList =
+  () => async (dispatch: Dispatch<Action<string>>) => {
+    try {
+      dispatch(fetchStarted());
+      const response = await fetch(SERVER_TRANSACTIONS_LIST_URL, {
+        method: "POST",
+        headers: {
+          Authorization: `${localStorage.getItem("jwt-token")}`,
+        },
       });
       const data = await response.json();
       if (!response.ok) throw new Error(data.error);
