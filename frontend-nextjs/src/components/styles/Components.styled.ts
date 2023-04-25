45b35aa7809a76cf3a059062c9c8e975e9f1b41d
Filipe <filipegallodev@hotmail.com>
2023-04-25 09:42:41 -0300

    feat: common styled-components in a separate files

---
+++ frontend-nextjs/src/components/styles/Components.styled.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.div`
+  width: 100%;
+`;
+
+export const SubTitle = styled.h2`
+  margin: 32px 0px;
+  font-size: 2rem;
+  text-transform: uppercase;
+  box-sizing: border-box;
+  &::before {
+    content: "";
+    width: 32px;
+    height: 4px;
+    background-color: #c500d0;
+    position: absolute;
+  }
+`;
+
+export const Button = styled.button`
+  background-color: #fa92ff;
+  margin: 8px 0px;
+  padding: 12px 24px;
+  border: none;
+  border-radius: 6px;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+  font-size: 1.25rem;
+  cursor: pointer;
+  transition: 0.1s;
+  &:disabled {
+    background-color: #e5e5e5;
+    cursor: not-allowed;
+  }
+  &:enabled:hover {
+    background-color: #f53fff;
+  }
+`;
