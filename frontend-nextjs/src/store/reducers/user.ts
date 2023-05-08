b9cc109cc62e407a414757b51f4e7ed3dead4720
Filipe <fgallo_dev@hotmail.com>
2023-05-08 11:09:28 -0300

    feat: functions used many times turned into a reusable function

--- frontend-nextjs/src/store/reducers/user.ts
+++ frontend-nextjs/src/store/reducers/user.ts
@@ -1,4 +1,5 @@
 import { Action, Dispatch, createSlice } from "@reduxjs/toolkit";
+import fetchData from "../utils/fetchData";
 
 const slice = createSlice({
   name: "user",
@@ -31,8 +32,8 @@ const slice = createSlice({
   },
 });
 
-export const { resetState } = slice.actions;
-const { fetchStarted, fetchSuccess, fetchError } = slice.actions;
+export const { fetchStarted, fetchSuccess, fetchError, resetState } =
+  slice.actions;
 const SERVER_VALIDATE_URL = "http://localhost:3333/token/validate";
 // const SERVER_VALIDATE_URL = "https://ng-cash-app-production.up.railway.app/token/validate";
 const SERVER_USER_UPDATE_URL = "http://localhost:3333/user/update";
@@ -44,80 +45,48 @@ const SERVER_USER_DELETE_URL = "http://localhost:3333/user/delete";
 
 export const fetchToken =
   (token: string) => async (dispatch: Dispatch<Action<string>>) => {
-    try {
-      dispatch(fetchStarted());
-      const response = await fetch(SERVER_VALIDATE_URL, {
-        method: "POST",
-        headers: {
-          Authorization: `${token}`,
-        },
-      });
-      const data = await response.json();
-      if (!response.ok) throw new Error("Error: " + data.error);
-      dispatch(fetchSuccess(data));
-    } catch (err) {
-      if (err instanceof Error) dispatch(fetchError(err.message));
-    }
+    await fetchData(dispatch, SERVER_VALIDATE_URL, {
+      method: "POST",
+      headers: {
+        Authorization: `${token}`,
+      },
+    });
   };
 
 export const fetchUserUpdate =
   (formData: IUserUpdateFormData) =>
   async (dispatch: Dispatch<Action<string>>) => {
-    try {
-      dispatch(fetchStarted());
-      const response = await fetch(SERVER_USER_UPDATE_URL, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${localStorage.getItem("jwt-token")}`,
-        },
-        body: JSON.stringify(formData),
-      });
-      const data = await response.json();
-      if (!response.ok) throw new Error("Error: " + data.error);
-      dispatch(fetchSuccess(data));
-    } catch (err) {
-      if (err instanceof Error) dispatch(fetchError(err.message));
-    }
+    await fetchData(dispatch, SERVER_USER_UPDATE_URL, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${localStorage.getItem("jwt-token")}`,
+      },
+      body: JSON.stringify(formData),
+    });
   };
 
 export const fetchEmailUpdate =
   (formData: IEmailUpdateFormData) =>
   async (dispatch: Dispatch<Action<string>>) => {
-    try {
-      dispatch(fetchStarted());
-      const response = await fetch(SERVER_EMAIL_UPDATE_URL, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${localStorage.getItem("jwt-token")}`,
-        },
-        body: JSON.stringify(formData),
-      });
-      const data = await response.json();
-      if (!response.ok) throw new Error("Error: " + data.error);
-      dispatch(fetchSuccess(data));
-    } catch (err) {
-      if (err instanceof Error) dispatch(fetchError(err.message));
-    }
+    await fetchData(dispatch, SERVER_EMAIL_UPDATE_URL, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${localStorage.getItem("jwt-token")}`,
+      },
+      body: JSON.stringify(formData),
+    });
   };
 
 export const fetchUserDelete =
   () => async (dispatch: Dispatch<Action<string>>) => {
-    try {
-      dispatch(fetchStarted());
-      const response = await fetch(SERVER_USER_DELETE_URL, {
-        method: "DELETE",
-        headers: {
-          Authorization: `${localStorage.getItem("jwt-token")}`,
-        },
-      });
-      const data = await response.json();
-      if (!response.ok) throw new Error("Error: " + data.error);
-      dispatch(fetchSuccess(data));
-    } catch (err) {
-      if (err instanceof Error) dispatch(fetchError(err.message));
-    }
+    await fetchData(dispatch, SERVER_USER_DELETE_URL, {
+      method: "DELETE",
+      headers: {
+        Authorization: `${localStorage.getItem("jwt-token")}`,
+      },
+    });
   };
 
 export default slice.reducer;
