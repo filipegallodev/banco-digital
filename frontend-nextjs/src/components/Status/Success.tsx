02f78a2de160e5cf5133a560b4b01fc5af6d53ac
Filipe <filipegallodev@hotmail.com>
2023-05-12 11:37:34 -0300

    feat: new success status

--- frontend-nextjs/src/components/Status/Success.tsx
+++ frontend-nextjs/src/components/Status/Success.tsx
@@ -1,9 +1,36 @@
-import React from "react";
-import { Alert, Fade } from "@mui/material";
-import styled from "styled-components";
+import React, { useState, useEffect, useCallback } from "react";
+import { Alert, AlertTitle, Fade } from "@mui/material";
+import styled, { keyframes } from "styled-components";
+import { useAppDispatch } from "@/hooks/useAppDispatch";
+import { clearStatus } from "@/store/reducers/user";
+import { resetLoginData } from "@/store/reducers/login";
+import { clearRegisterStatus } from "@/store/reducers/register";
+import { clearTransactionStatus } from "@/store/reducers/transactions";
 
 const Success = ({ message }: { message: string | undefined | null }) => {
-  if (!message) return null;
+  const dispatch = useAppDispatch();
+  const [showAlert, setShowAlert] = useState(true);
+
+  useEffect(() => {
+    if (message) setShowAlert(true);
+  }, [message]);
+
+  const handleClose = useCallback(() => {
+    dispatch(clearStatus());
+    dispatch(resetLoginData());
+    dispatch(clearRegisterStatus());
+    dispatch(clearTransactionStatus());
+  }, [dispatch]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowAlert(false);
+      handleClose();
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, [showAlert, handleClose]);
+
+  if (!message || !showAlert) return null;
   return (
     <Container>
       <Fade in={true}>
@@ -12,16 +39,44 @@ const Success = ({ message }: { message: string | undefined | null }) => {
           sx={{
             boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
           }}
+          onClose={handleClose}
+          className="fadeIn alert-container"
         >
-          {message}
+          <AlertTitle>
+            <strong>Sucesso</strong>
+          </AlertTitle>
+          <TextSuccess>{message}</TextSuccess>
         </Alert>
       </Fade>
     </Container>
   );
 };
 
+const FadeAnimation = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(20px);
+  }
+  to {
+    opacity: initial;
+    transform: initial;
+  }
+`;
+
 const Container = styled.div`
-  margin: 8px 0px;
+  position: fixed;
+  top: 16px;
+  right: 16px;
+  z-index: 9999;
+  border-radius: 4px;
+  animation: ${FadeAnimation} 0.75s;
+  &.alert-container {
+    box-shadow: 0px 0px 0px 2px rgba(255, 120, 120, 1);
+  }
+`;
+
+const TextSuccess = styled.p`
+  font-size: 1rem;
 `;
 
 export default Success;
