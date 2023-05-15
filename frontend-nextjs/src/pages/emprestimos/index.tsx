c2110e92c4053ce9a95c841211071ba94d8d6ad6
Filipe <fgallo_dev@hotmail.com>
2023-05-15 10:11:52 -0300

    feat: basic loan page with slider and input

--- frontend-nextjs/src/pages/emprestimos/index.tsx
+++ frontend-nextjs/src/pages/emprestimos/index.tsx
@@ -2,12 +2,49 @@ import AuthPage from "@/components/AuthPage";
 import Header from "@/components/Header";
 import ReturnButton from "@/components/ReturnButton";
 import useTokenAuthentication from "@/hooks/useTokenAuthentication";
+import { Slider } from "@mui/material";
 import Head from "next/head";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CurrencyInput from "react-currency-input-field";
 import styled from "styled-components";
 
 export default function Emprestimos() {
   const user = useTokenAuthentication();
+  const [loan, setLoan] = useState<number>();
+  const [customLoan, setCustomLoan] = useState<number>(50);
+
+  useEffect(() => {
+    if (user.data.user?.loan) {
+      const loanNumber = Number(
+        user.data.user.loan
+          .replace(/\D{2}\s/g, "")
+          .replaceAll(".", "")
+          .replace(",", ".")
+      );
+      setLoan(loanNumber);
+      if (loan === 0 || loan === undefined) setCustomLoan(loanNumber);
+    }
+  }, [user]);
+
+  function handleSliderChange(event: Event, newValue: number | number[]) {
+    if (typeof newValue === "number") {
+      setCustomLoan(newValue);
+    }
+  }
+
+  function handleInputChange(value: string | undefined) {
+    const valueNumber = Number(value);
+    if (!loan) return;
+    if (valueNumber <= loan) {
+      setCustomLoan(valueNumber);
+    }
+    if (valueNumber > loan) {
+      setCustomLoan(25000);
+    }
+    if (valueNumber < 50) {
+      setCustomLoan(50);
+    }
+  }
 
   if (!user.data) return <AuthPage />;
   return (
@@ -23,6 +60,38 @@ export default function Emprestimos() {
         <Container>
           <ReturnButton />
           <Title>Empréstimos</Title>
+          {loan && (
+            <>
+              <p>
+                {customLoan
+                  ? customLoan.toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    })
+                  : loan.toLocaleString("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
+              </p>
+              <CurrencyInput
+                id="custom-loan-value"
+                name="custom-loan-value"
+                placeholder="R$ 0,00"
+                prefix="R$ "
+                decimalsLimit={2}
+                value={customLoan}
+                onValueChange={(value) => handleInputChange(value)}
+              />
+              <Slider
+                aria-label="Loan"
+                value={customLoan}
+                min={50}
+                step={50}
+                max={loan}
+                onChange={handleSliderChange}
+              />
+            </>
+          )}
         </Container>
       </main>
     </>
@@ -34,7 +103,7 @@ const Container = styled.section`
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
-  margin: 0 auto;
+  margin: 0 auto 24px auto;
   max-width: 1200px;
 `;
 
