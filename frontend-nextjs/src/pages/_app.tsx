97e812451de2c9baf9d1bc47b6ce9a712697cba5
Filipe <fgallo_dev@hotmail.com>
2023-06-07 12:24:29 -0300

    fix: styled-components ssr

--- frontend-nextjs/src/pages/_app.tsx
+++ frontend-nextjs/src/pages/_app.tsx
@@ -5,17 +5,14 @@ import store from "@/store/configureStore";
 import { ThemeProvider } from "styled-components";
 import Theme from "@/styles/Theme";
 import GlobalStyle from "@/styles/GlobalStyle";
-import StyledComponentsRegistry from "@/lib/registry";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <StyledComponentsRegistry>
-        <ThemeProvider theme={Theme}>
-          <GlobalStyle />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </StyledComponentsRegistry>
+      <ThemeProvider theme={Theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </Provider>
   );
 }
