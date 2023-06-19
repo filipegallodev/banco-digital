c8cac5e2a2fd3294c109fe20d18dc5bd735ef3d3
Filipe <fgallo_dev@hotmail.com>
2023-06-19 10:08:40 -0300

    update: new page and page returning after loan success

--- frontend-nextjs/src/pages/emprestimos/novo/index.tsx
+++ frontend-nextjs/src/pages/emprestimos/novo/index.tsx
@@ -4,17 +4,18 @@ import ReturnButton from "@/components/ReturnButton";
 import useTokenAuthentication from "@/hooks/useTokenAuthentication";
 import Head from "next/head";
 import React from "react";
+import LoanSection from "@/components/Loan/LoanSection";
 import SectionContainer from "@/components/Section/SectionContainer";
 import SectionTitle from "@/components/Section/SectionTitle";
 import Footer from "@/components/Footer";
 import { useAppSelector } from "@/hooks/useAppSelector";
-import { useRouter } from "next/router";
-import * as Styled from "@/components/styles/Components.styled";
+import { Backdrop, CircularProgress } from "@mui/material";
+import Success from "@/components/Status/Success";
+import Error from "@/components/Status/Error";
 
 export default function Emprestimos() {
   const user = useTokenAuthentication();
   const loan = useAppSelector((state) => state.loan);
-  const router = useRouter();
 
   if (!user.data) return <AuthPage />;
   return (
@@ -29,13 +30,23 @@ export default function Emprestimos() {
       <main className="animeRight">
         <SectionContainer>
           <ReturnButton />
-          <SectionTitle>Empréstimos</SectionTitle>
-          <Styled.Button onClick={() => router.push("/emprestimos/novo")}>
-            Solicitar empréstimo
-          </Styled.Button>
+          <SectionTitle>Novo Empréstimo</SectionTitle>
+          <LoanSection />
         </SectionContainer>
       </main>
       <Footer />
+      <Backdrop
+        sx={{
+          color: "#fff",
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+          cursor: "wait",
+        }}
+        open={loan.loading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Success message={loan.data.status} />
+      <Error message={loan.error} />
     </>
   );
 }
