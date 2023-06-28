6a516b2d05eb05dde96c2cf637173e0b2be587ba
Filipe <fgallo_dev@hotmail.com>
2023-06-28 09:51:06 -0300

    fix: using card route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import userRouter from "./routes/user.route";
 import transactionRouter from "./routes/transaction.route";
 import cors from "cors";
 import loanRouter from "./routes/loan.route";
+import cardRouter from "./routes/card.route";
 
 const port = process.env.PORT || 3333;
 const server = express();
@@ -12,6 +13,7 @@ server.use(express.json());
 server.use(userRouter);
 server.use(loanRouter);
 server.use(transactionRouter);
+server.use(cardRouter);
 
 server.listen(port, () => {
   console.log(`[SERVIDOR] Rodando na porta ${port}`);
