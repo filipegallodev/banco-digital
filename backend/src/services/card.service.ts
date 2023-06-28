5cccb6155ef2dd8610194ffd4639500ced6e6bf2
Filipe <fgallo_dev@hotmail.com>
2023-06-28 09:21:41 -0300

    update: card service and util

--- backend/src/services/card.service.ts
+++ backend/src/services/card.service.ts
@@ -10,6 +10,13 @@ export async function newCard(
   const userId = checkAuth(authorization);
   const dbUser = await PrismaUtil.findUser("id", userId);
   if (!dbUser) return { status: "Usuário não encontrado.", success: false };
+  const cards = await CardUtil.findCards(dbUser);
+  const result = cards?.find((card) => card.type === cardData.type);
+  if (result)
+    return {
+      status: `Você já possui um cartão ${cardData.type.toUpperCase()}.`,
+      success: false,
+    };
   await CardUtil.createCard(cardData, dbUser);
   return { status: "Cartão solicitado com sucesso.", success: true };
 }
--- backend/src/utils/card.util.ts
+++ backend/src/utils/card.util.ts
@@ -4,5 +4,31 @@ import { ICard } from "../types/card";
 const prisma = new PrismaClient();
 
 export async function createCard(cardData: ICard, user: User) {
-  return false;
+  try {
+    await prisma.card.create({
+      data: {
+        type: cardData.type,
+        number: cardData.number,
+        validity: cardData.validity,
+        invoiceClosing: cardData.invoiceClosing,
+        accountId: user.accountId,
+      },
+    });
+    return true;
+  } catch (error) {
+    return;
+  }
+}
+
+export async function findCards(user: User) {
+  try {
+    const cards = await prisma.card.findMany({
+      where: {
+        accountId: user.accountId,
+      },
+    });
+    return cards;
+  } catch (error) {
+    return;
+  }
 }
